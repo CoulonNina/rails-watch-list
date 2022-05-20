ea0cadaa507b91dd0ca456a69ed36f2ab3a901e5
Nina Coulon <ninacoulon@ymail.com>
2022-05-20 11:13:40 +0200

    e

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,20 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+require "json"
+require "open-uri"
+
+Movie.destroy_all if Rails.env.development?
+
+
+response = URI.open("http://tmdb.lewagon.com/movie/top_rated").read
+repos = JSON.parse(response)
+
+repos["results"].each do |e|
+    Movie.create(
+        title: e["title"],
+        overview: e["overview"],
+        poster_url: "https://image.tmdb.org/t/p/w500#{e["backdrop_path"]}",
+        rating: e["vote_average"],
+    )
+end
